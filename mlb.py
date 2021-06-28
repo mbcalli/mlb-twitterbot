4321480fa2f05a8f6ac5c3cca4bde0baa572e1e5
mbcalli <mbcalli@clemson.edu>
2021-06-28 15:25:15 -0400

    Update mlb.py

--- mlb.py
+++ mlb.py
@@ -1,41 +1,51 @@
 #!/usr/bin/env python3
-import statsapi
+import sys
+import time
 import datetime
 from datetime import datetime
 import csv
+
+import statsapi
 from twython import Twython
-import sys
 from twython import TwythonError
-import time
 import mlb_grapher
 import config
 
+
+# Reads in which team to analyze from the Cron job;  example: 'atl'
 team_code = sys.argv[1]
 
+# Gets the team id from the team_code
 team_lookup = statsapi.lookup_team(team_code)
 for team in team_lookup:
     teamCode = team['fileCode']
     if teamCode == team_code:
         team_id = team['id']
 
+# Filepath to the team info which stores team name, colors, etc.
 mlb_team_info_filepath = '/home/pi/twitterbot/mlb_team_info.csv'
+
+# Get team info for each team -- easier than looking for specific team
 mlb_team_info = []
 with open(mlb_team_info_filepath, mode='r', encoding='utf-8-sig') as file:
     reader = csv.reader(file)
     for row in reader:
         mlb_team_info.append(list(row))
 
-
+# Get the schedule for the team on a given date
 def schedule_on_date(team_id, date):
     sch = statsapi.schedule(team=team_id, start_date=date, end_date=date)
     return sch
 
+# Get the home and away team win probability at the moment the function is called
 def get_win_probs(game_id):
     data = statsapi.get('game_contextMetrics', {'gamePk': game_id})
     win_prob = round(data['homeWinProbability'], 2) # Win probability of the home team
 
     return win_prob, 100-win_prob
 
+
+# Get the win probability string (which is a formatted string, ready to be posted to twitter)
 def get_win_probability_string(game_id, home_name, away_name):
     home_win_prob, away_win_prob = get_win_probs(game_id)
     if home_win_prob >= 50:
@@ -43,6 +53,7 @@ def get_win_probability_string(game_id, home_name, away_name):
     else:
         return away_name + ' have a ' + str(away_win_prob) + '% chance of winning.'   
 
+# Get the formatted inning (ready to be tweeted)
 def format_inning(current_inning, inning_state):
     inning_formats = {
         1: '1st',
@@ -56,16 +67,21 @@ def format_inning(current_inning, inning_state):
     inning_state = inning_state
     return inning_state + ' of the ' + current_inning
 
+
+# Get the formatted score (ready to be tweeted)
 def format_score(home_name, away_name, home_score, away_score):
     return home_name + '   ' + str(home_score) + '\nvs.\n' + away_name + '   ' + str(away_score)
 
-
+# Start the Twython api
 def twython_api_start():
     return Twython(config.CONSUMER_KEY, config.CONSUMER_SECRET, config.ACCESS_KEY, config.ACCESS_SECRET)
 
+
+# Use Twython to update twitter status
 def twython_update_status(api, text):
     api.update_status(status=text)
 
+# Update twitter status with formatted strings
 def update_status(text, game_id, formatted_inning):
     api = twython_api_start()
     try:
@@ -74,13 +90,14 @@ def update_status(text, game_id, formatted_inning):
     except TwythonError:
         print('Tweet already posted: ' + text)
         
-
+# Writes the win probability to a csv file so that historical info can be retrieved by mlb_grapher.py
 def write_win_probability(win_prob_filepath, game_id, time, home_name, away_name, inning_state):
     home_win_prob, away_win_prob = get_win_probs(game_id)
     with open(win_prob_filepath, mode='a') as file:
         writer = csv.writer(file, delimiter=',')
         writer.writerow([time, home_win_prob, away_win_prob, home_name, away_name, inning_state])
 
+# Writes the win probability to a csv file so that historical info can be retrieved by mlb_grapher.py
 def write_win_prob_end_of_game(win_prob_filepath, game_id, time, home_name, away_name, home_team_won):
     with open(win_prob_filepath, mode='a') as file:
         writer = csv.writer(file, delimiter=',')
@@ -89,16 +106,12 @@ def write_win_prob_end_of_game(win_prob_filepath, game_id, time, home_name, away
         else:
             writer.writerow([time, 0, 100, home_name, away_name])
 
+# Converts UTC time to Local time
 def datetime_from_utc_to_local(utc_datetime):
-    print(utc_datetime)
     utc_datetime = datetime.strptime(utc_datetime, '%H:%M:%S')
-    print(utc_datetime)
     now_timestamp = time.time()
-    print(now_timestamp)
     offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
-    print(offset)
     est_time =  utc_datetime + offset
-    print(est_time)
     return datetime.strftime(est_time, '%I:%M %p')
 
 # Select the correct hashtags for tweet
@@ -113,16 +126,21 @@ def get_formatted_hashtags(home_name, away_name, mlb_team_info):
             away_hashtags += team[2] + ' ' + team[3]
     return home_hashtags, away_hashtags
 
+# Get the relevant info for today's game
 now = datetime.now()
 today = now.strftime("%m/%d/%Y")
 game_time = now.strftime("%H:%M:%S")
 schedule = schedule_on_date(team_id, today)
 
-
+# If no game today
 if len(schedule) == 0:
     pass
+# If game today
 else:
+    # Loop through each game that team is playing today (may be double header)
     for game in schedule:
+
+        # Get game info from api
         game_id = game['game_id']
         is_home_team = game['home_id'] == team_id
         inning_state = game['inning_state']
@@ -131,23 +149,41 @@ else:
         away_name = game['away_name']
         home_score = game['home_score']
         away_score = game['away_score']
+
+        # Determine if team is home or away team
         if is_home_team:
             my_team_name = home_name
             other_team_name = away_name
         else:
             my_team_name = away_name
             other_team_name = home_name
+
+        # Gets hashtags for today's game
         home_hashtags, away_hashtags = get_formatted_hashtags(home_name, away_name, mlb_team_info)
+
+        # Determine if game is currently in progress
         if game['status'] == 'In Progress':
+
+            # Get win probability and write to csv
             win_probability_string = get_win_probability_string(game_id, home_name, away_name)
             write_win_probability('/home/pi/twitterbot/win_probs/' + str(game_id) + '.csv', game_id, game_time, home_name, away_name, inning_state)
+            
+            # Generate updated graph using mlb_grapher.py
             mlb_grapher.generate_graph(str(game_id))
+            
+            # If ballgame has just ended
             if inning_state == 'End':
+
+                # Get info and tweak string to match end of game scenario then tweet
                 formatted_inning = format_inning(current_inning, inning_state)
                 formatted_score = format_score(home_name, away_name, home_score, away_score)
                 text = formatted_inning + ':\n\n' + formatted_score + '\n\n' + win_probability_string + '\n\n' + '#MLB (' + str(game_id) + ')\n'  + home_hashtags + '\n' + away_hashtags
                 update_status(text, game_id, formatted_inning)
+
+        # If game is over
         elif game['status'] == 'Game Over':
+
+            # Get info and tweak string to match end of game scenario then tweet and generate graph
             formatted_inning = 'Final'
             formatted_score = format_score(home_name, away_name, home_score, away_score)
             win_probability = get_win_probability_string(game_id, home_name, away_name)
@@ -156,7 +192,11 @@ else:
             home_team_won = home_score > away_score
             write_win_prob_end_of_game('/home/pi/twitterbot/win_probs/' + str(game_id) + '.csv', game_id, game_time, home_name, away_name, home_team_won)
             mlb_grapher.generate_graph(str(game_id))
+
+        # If game hasn't started
         elif game['status'] == 'Pre-Game':
+
+            # Get info and tweak string to match pre game scenario then tweet
             game_datetime = game['game_datetime']
             game_datetime = game_datetime[11:19]
             game_time = datetime_from_utc_to_local(game_datetime)
